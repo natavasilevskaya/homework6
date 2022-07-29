ba29b6e2540cf350ed2f5371f609021fb756d2e4
natavasilevskaya <107160563+natavasilevskaya@users.noreply.github.com>
2022-07-29 21:17:51 +0300

    Update Program.cs

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -8,10 +8,13 @@ void InputCoefficients()
 {
   for (int i = 0; i < coeff.GetLength(0); i++)
   {
-    Console.Write($"Input the coefficients of the first equation {i+1}-he first equation (y = k * x + b):\n");
+    Console.Write($"Input the coefficients  {i+1} of the first equation (y = k * x + b):\n");
     for (int j = 0; j < coeff.GetLength(1); j++)
     {
-      if(j==0) Console.Write($"Input the coefficient k: ");
+      if(j==0) 
+      {
+        Console.Write($"Input the coefficient k: ");
+      }
       else Console.Write($"the coefficient b: ");
       coeff[i,j] = Convert.ToInt32(Console.ReadLine());
     }
