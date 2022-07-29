a8865c1297530f66a4cf06e09280e84fb3038f85
natavasilevskaya <107160563+natavasilevskaya@users.noreply.github.com>
2022-07-29 20:57:24 +0300

    Task2 file added

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Программа находит точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double[,] coeff = new double[2, 2];
+double[] crossPoint = new double[2];
+
+void InputCoefficients(){
+  for (int i = 0; i < coeff.GetLength(0); i++)
+  {
+    Console.Write($"Input the coefficients of the first equation {i+1}-he first equation (y = k * x + b):\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Input the coefficient k: ");
+      else Console.Write($"the coefficient b: ");
+      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Decision(double[,] coeff)
+{
+  crossPoint[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  crossPoint[1] = crossPoint[0] * coeff[0,0] + coeff[0,1];
+  return crossPoint;
+}
+
+void OutputResponse(double[,] coeff)
+{
+    Decision(coeff);
+    Console.Write($"Point of intersection of lines ({crossPoint[0]}, {crossPoint[1]})");
+}
+
+InputCoefficients();
+OutputResponse(coeff);
